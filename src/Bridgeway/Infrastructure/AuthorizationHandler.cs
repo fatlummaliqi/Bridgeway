f3c3ab97b878286254af3b7fff9a6e972eaa4832
Fatlum Maliqi <fatlum@gjirafa.com>
2024-09-01 12:48:17 +0200

    Implement AuthorizationHandler

--- src/Bridgeway/Infrastructure/AuthorizationHandler.cs
+++ src/Bridgeway/Infrastructure/AuthorizationHandler.cs
@@ -1,5 +1,41 @@
-﻿namespace Bridgeway;
+﻿using Microsoft.Extensions.DependencyInjection;
 
-internal class AuthorizationHandler
+namespace Bridgeway;
+
+internal class AuthorizationHandler(IServiceProvider serviceProvider) : DelegatingHandler
 {
+    private static readonly HttpRequestOptionsKey<string> RequestOptionsKey = new(BridgewayDefaults.BrokerRequestOptionKey);
+
+    protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (request.Options.TryGetValue(RequestOptionsKey, out var brokerAlias))
+        {
+            var authorizationStrategy = serviceProvider.GetKeyedService<IAuthorizationStrategy>(brokerAlias);
+
+            if (authorizationStrategy != null)
+            {
+                authorizationStrategy
+                    .AuthorizeAsync(request, cancellationToken)
+                    .GetAwaiter()
+                    .GetResult();
+            }
+        }
+
+        return base.Send(request, cancellationToken);
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        if (request.Options.TryGetValue(RequestOptionsKey, out var brokerAlias))
+        {
+            var authorizationStrategy = serviceProvider.GetKeyedService<IAuthorizationStrategy>(brokerAlias);
+
+            if (authorizationStrategy != null)
+            {
+                await authorizationStrategy.AuthorizeAsync(request, cancellationToken);
+            }
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
 }
