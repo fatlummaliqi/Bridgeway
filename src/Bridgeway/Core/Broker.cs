e20ed235913d5bef31467996ea71d7aed8d048bb
Fatlum Maliqi <fatlum@gjirafa.com>
2024-09-01 13:02:45 +0200

    Add ability to extract parsed payload out of http response

--- src/Bridgeway/Core/Broker.cs
+++ src/Bridgeway/Core/Broker.cs
@@ -33,4 +33,28 @@ public abstract class Broker(IHttpClientFactory httpClientFactory)
 
         return response;
     }
+
+    protected virtual T Request<T>(string path, HttpMethod method, BaseOptions? options)
+    {
+        var request = options.BuildRequest(path, method, Alias);
+
+        var client = httpClientFactory.CreateClient(Alias);
+
+        var response = client.Send(request);
+
+        return (T)HttpContentUtils.ProcessPayloadAsync(response.Content, typeof(T))
+            .GetAwaiter()
+            .GetResult();
+    }
+
+    protected virtual async Task<T> RequestAsync<T>(string path, HttpMethod method, BaseOptions options, CancellationToken cancellationToken)
+    {
+        var request = options.BuildRequest(path, method, Alias);
+
+        var client = httpClientFactory.CreateClient(Alias);
+
+        var response = await client.SendAsync(request, cancellationToken);
+
+        return (T)await HttpContentUtils.ProcessPayloadAsync(response.Content, typeof(T), cancellationToken);
+    }
 }
--- src/Bridgeway/Utils/HttpContentUtils.cs
+++ src/Bridgeway/Utils/HttpContentUtils.cs
@@ -1,5 +1,32 @@
-﻿namespace Bridgeway;
+﻿using System.Net.Mime;
+using System.Text.Json;
+
+namespace Bridgeway;
 
 internal static class HttpContentUtils
 {
+    public static async Task<object> ProcessPayloadAsync(HttpContent content, Type destinationType, CancellationToken cancellationToken = default)
+    {
+        var contentType = content!.Headers!.ContentType!.MediaType;
+
+        return contentType switch
+        {
+            MediaTypeNames.Application.Json => await ParseJsonAsync(content, destinationType, cancellationToken),
+            _ => throw new NotSupportedException("Not supported response payload content type.")
+        };
+    }
+
+    private static async Task<object> ParseJsonAsync(HttpContent content, Type destinationType, CancellationToken cancellationToken)
+    {
+        var payload = await content.ReadAsStringAsync(cancellationToken);
+
+        try
+        {
+            return JsonSerializer.Deserialize(payload, destinationType)!;
+        }
+        catch (Exception ex)
+        {
+            throw new BrokerException("Failed processing json encoded response payload.", ex);
+        }
+    }
 }
