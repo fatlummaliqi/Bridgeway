bcbe6221107fc6af35df8eb1c62c01d349f518cc
Fatlum Maliqi <fatlum@gjirafa.com>
2024-09-02 22:05:44 +0200

    Add unit tests for BaseOptionsExtensions

---
+++ test/Bridgeway.Test.Unit/BaseOptionsExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Bridgeway.Test.Unit.Helpers.Options;
+
+namespace Bridgeway.Test.Unit;
+
+public class BaseOptionsExtensionsTests
+{
+    public const string UrlPath = "/api/v1/dummy-resource-path";
+    public const string BrokerAlias = "broker-aliasp-example";
+
+    [Fact]
+    public void BuildRequest_ShouldReturn_ExceptedResult_WhenOptionsNull()
+    {
+        //arrange
+        BaseOptions? options = null;
+
+        //act
+        var request = options.BuildRequest(UrlPath, HttpMethod.Get, BrokerAlias);
+
+        //assert
+        Assert.NotNull(request);
+        Assert.Equal(UrlPath, request.RequestUri!.ToString());
+        Assert.Equal(HttpMethod.Get, request.Method);
+        Assert.True(request.Options.TryGetValue(new HttpRequestOptionsKey<string>(BridgewayDefaults.BrokerRequestOptionKey), out var option) && option == BrokerAlias);
+    }
+
+    [Fact]
+    public void BuildRequest_ShouldThrow_NotSupportedException()
+    {
+        //arrange
+        var options = new NotEncodableOptions();
+
+        //act
+        Action action = () => options.BuildRequest(UrlPath, HttpMethod.Get, BrokerAlias);
+
+        //assert
+        Assert.Throws<NotSupportedException>(action);
+    }
+}
